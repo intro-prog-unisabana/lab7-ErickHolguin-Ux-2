4d25c33bd3cec632ca35dc62f333fc9cfe4c38ee
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-04-09 11:13:24 -0500

    Complete 2do

--- password_manager.py
+++ password_manager.py
@@ -15,11 +15,19 @@ def encrypt_single_pass(filename: str) -> None:
 
 def encrypt_passwords_in_file(filename: str) -> None:
     """TODO: Parte 2."""
-    
+
     with open(filename, mode='r') as file:
         reader = csv.reader(file)
+        rows = []
         for row in reader:
+            if row[0] != "website":
+                row[2] = caesar_encrypt(row[2])
+            rows.append(row)
             print(row)
+        return rows
+
+         
+         
 
 
 def change_password(filename: str, website: str, password: str) -> bool:
