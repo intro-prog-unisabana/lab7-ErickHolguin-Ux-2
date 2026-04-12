5673c745d47277d61ac848818947fd45bb73f04a
Erick Fernando Holguin Ceron <subtanicdos3@gmail.com>
2026-04-11 22:21:40 -0500

    parte 5, try 3

--- main.py
+++ main.py
@@ -7,6 +7,26 @@ def main() -> None:
     filename = input("Enter the CSV file name:\n")
     encrypt_all_passwords(filename)
 
+    while True:
+        option = input("Options: (1) Change Password, (2) Add Password, (3) Quit:\n")
+
+        if option == "1":
+            website, new_pass = input("Enter the website and the new password:\n").split()
+            if change_password(filename, website, new_pass):
+                print("Password changed.")
+            else:
+                print("Website not found! Operation failed.")
+        
+        elif option == "2":
+            website, user, passwd = input("Enter the website, username, and password:\n").split()
+            add_login(filename, website, user, passwd)
+            print("Login added.")
+
+        elif option == "3":
+            break
+        else:
+            print("Invalid option selected!")
+
 
 if __name__ == "__main__":
     main()
